0be310ae9f3dcb4c67a3a639110516228b09a019
oakes <zach@sekao.net>
2019-04-13 07:12:05 -0500

    Add copyright renunciation

--- project.clj
+++ project.clj
@@ -1,4 +1,6 @@
 (defproject paravim "0.1.0-SNAPSHOT"
+  :license {:name "Public Domain"
+            :url "http://unlicense.org/UNLICENSE"}
   :repositories [["clojars" {:url "https://clojars.org/repo"
                              :sign-releases false}]]
   :clean-targets ^{:protect false} ["target"]
