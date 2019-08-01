bde37d82f1d26b01a99742c130f10c3c1192d191
oakes <zach@sekao.net>
2019-08-01 18:39:15 -0400

    First attempt at calling libvim functions

--- src/paravim/start.clj
+++ src/paravim/start.clj
@@ -4,7 +4,9 @@
   (:import  [org.lwjgl.glfw GLFW Callbacks GLFWCursorPosCallbackI GLFWKeyCallbackI]
             [org.lwjgl.opengl GL GL41]
             [org.lwjgl.system MemoryUtil]
-            [javax.sound.sampled AudioSystem Clip])
+            [javax.sound.sampled AudioSystem Clip]
+            [org.lwjgl.system Library CallbackI$V]
+            [org.lwjgl.system.dyncall DynCall DynCallback])
   (:gen-class))
 
 (defn listen-for-mouse [window]
@@ -51,6 +53,46 @@
             nil))))))
 
 (defn -main [& args]
+  (let [lib (Library/loadNative "libvim")
+        init (.getFunctionAddress lib "vimInit")
+        set-tab-size (.getFunctionAddress lib "vimOptionSetTabSize")
+        get-tab-size (.getFunctionAddress lib "vimOptionGetTabSize")
+        set-quit (.getFunctionAddress lib "vimSetQuitCallback")
+        exec (.getFunctionAddress lib "vimExecute")
+        vm (DynCall/dcNewCallVM 1024)
+        callback (reify CallbackI$V
+                   (callback [this args]
+                     (println "quit")
+                     (println (DynCallback/dcbArgPointer args))
+                     (println (DynCallback/dcbArgBool args)))
+                   (getSignature [this]
+                     "(pb)v"))]
+    (DynCall/dcMode vm DynCall/DC_CALL_C_DEFAULT)
+
+    (DynCall/dcReset vm)
+    (DynCall/dcCallVoid vm init)
+
+    (DynCall/dcReset vm)
+    (DynCall/dcArgInt vm 2)
+    (DynCall/dcCallVoid vm set-tab-size)
+
+    (DynCall/dcReset vm)
+    (println (DynCall/dcCallInt vm get-tab-size))
+
+    (DynCall/dcReset vm)
+    (DynCall/dcArgPointer vm (-> "set tabstop=4" MemoryUtil/memUTF8 MemoryUtil/memAddress))
+    (DynCall/dcCallVoid vm exec)
+
+    (DynCall/dcReset vm)
+    (println (DynCall/dcCallInt vm get-tab-size))
+
+    (DynCall/dcReset vm)
+    (DynCall/dcArgPointer vm (MemoryUtil/memAddressSafe callback))
+    (DynCall/dcCallVoid vm set-quit)
+
+    (DynCall/dcReset vm)
+    (DynCall/dcArgPointer vm (-> "q!" MemoryUtil/memUTF8 MemoryUtil/memAddress))
+    (DynCall/dcCallVoid vm exec))
   (when-not (GLFW/glfwInit)
     (throw (Exception. "Unable to initialize GLFW")))
   (GLFW/glfwWindowHint GLFW/GLFW_VISIBLE GLFW/GLFW_FALSE)
