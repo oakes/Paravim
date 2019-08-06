114cf5a9019411ed1fefb847a5d2d576550654df
oakes <zach@sekao.net>
2019-08-06 12:22:40 -0400

    Reorder methods

--- src/paravim/vim.clj
+++ src/paravim/vim.clj
@@ -5,35 +5,33 @@
 
 (defprotocol IVim
   (init [this])
-  (set-tab-size [this size])
-  (get-tab-size [this])
+  ;; user input
   (execute [this cmd])
-  (set-quit [this callback]))
+  ;; callbacks
+  (set-quit [this callback])
+  ;; options
+  (set-tab-size [this size])
+  (get-tab-size [this]))
 
 (defn ->vim []
   (let [lib (Library/loadNative "libvim")
         init (.getFunctionAddress lib "vimInit")
+        exec (.getFunctionAddress lib "vimExecute")
+        set-quit (.getFunctionAddress lib "vimSetQuitCallback")
         set-tab-size (.getFunctionAddress lib "vimOptionSetTabSize")
         get-tab-size (.getFunctionAddress lib "vimOptionGetTabSize")
-        set-quit (.getFunctionAddress lib "vimSetQuitCallback")
-        exec (.getFunctionAddress lib "vimExecute")
         vm (DynCall/dcNewCallVM 1024)]
     (reify IVim
       (init [this]
         (DynCall/dcMode vm DynCall/DC_CALL_C_DEFAULT)
         (DynCall/dcReset vm)
         (DynCall/dcCallVoid vm init))
-      (set-tab-size [this size]
-        (DynCall/dcReset vm)
-        (DynCall/dcArgInt vm size)
-        (DynCall/dcCallVoid vm set-tab-size))
-      (get-tab-size [this]
-        (DynCall/dcReset vm)
-        (DynCall/dcCallInt vm get-tab-size))
+      ;; user input
       (execute [this cmd]
         (DynCall/dcReset vm)
         (DynCall/dcArgPointer vm (-> cmd MemoryUtil/memUTF8 MemoryUtil/memAddress))
         (DynCall/dcCallVoid vm exec))
+      ;; callbacks
       (set-quit [this callback]
         (DynCall/dcReset vm)
         (DynCall/dcArgPointer vm (MemoryUtil/memAddressSafe
@@ -43,5 +41,13 @@
                                                  (DynCallback/dcbArgBool args)))
                                      (getSignature [this]
                                        "(pb)v"))))
-        (DynCall/dcCallVoid vm set-quit)))))
+        (DynCall/dcCallVoid vm set-quit))
+      ;; options
+      (set-tab-size [this size]
+        (DynCall/dcReset vm)
+        (DynCall/dcArgInt vm size)
+        (DynCall/dcCallVoid vm set-tab-size))
+      (get-tab-size [this]
+        (DynCall/dcReset vm)
+        (DynCall/dcCallInt vm get-tab-size)))))
 
