c9348624a48d91b806afb2ee34e59d36d39ace3f
oakes <zach@sekao.net>
2019-09-08 20:55:29 -0400

    Refactor to make it possible to embed in a play-cljc game

--- src/paravim/start.clj
+++ src/paravim/start.clj
@@ -13,24 +13,6 @@
             [javax.sound.sampled AudioSystem Clip])
   (:gen-class))
 
-(defn listen-for-resize [window game]
-  (GLFW/glfwSetWindowSizeCallback window
-    (reify GLFWWindowSizeCallbackI
-      (invoke [this window width height]
-        (swap! c/*state
-               (fn [{:keys [current-buffer current-tab tab->buffer] :as state}]
-                 (as-> state state
-                       (if current-buffer
-                         (update-in state [:buffers current-buffer] c/update-cursor state game)
-                         state)
-                       ;; if we're in the repl, make sure both the input and output are refreshed
-                       (if-let [other-tab (case current-tab
-                                            :repl-in :repl-out
-                                            :repl-out :repl-in
-                                            nil)]
-                         (update-in state [:buffers (tab->buffer other-tab)] c/update-cursor state game)
-                         state))))))))
-
 (defn get-density-ratio [window]
   (let [*fb-width (MemoryUtil/memAllocInt 1)
         *window-width (MemoryUtil/memAllocInt 1)
@@ -41,45 +23,6 @@
     (MemoryUtil/memFree *fb-width)
     (MemoryUtil/memFree *window-width)
     (float (/ fb-width window-width))))
-
-(defn listen-for-mouse [window game vim pipes]
-  (GLFW/glfwSetCursorPosCallback window
-    (reify GLFWCursorPosCallbackI
-      (invoke [this window xpos ypos]
-        (as-> (swap! c/*state
-                (fn [state]
-                  (let [density-ratio (get-density-ratio window)
-                        x (* xpos density-ratio)
-                        y (* ypos density-ratio)]
-                    (c/update-mouse state game x y))))
-              state
-              (GLFW/glfwSetCursor window
-                                  (GLFW/glfwCreateStandardCursor
-                                    (case (:mouse-type state)
-                                      ;:ibeam GLFW/GLFW_IBEAM_CURSOR
-                                      :hand GLFW/GLFW_HAND_CURSOR
-                                      GLFW/GLFW_ARROW_CURSOR)))))))
-  (GLFW/glfwSetMouseButtonCallback window
-    (reify GLFWMouseButtonCallbackI
-      (invoke [this window button action mods]
-        (when (and (= button GLFW/GLFW_MOUSE_BUTTON_LEFT)
-                   (= action GLFW/GLFW_PRESS))
-          (vim/open-buffer-for-tab! vim (swap! c/*state c/click-mouse game
-                                               (fn [state]
-                                                 (let [{:keys [current-tab current-buffer buffers tab->buffer]} state
-                                                       {:keys [out-pipe]} pipes
-                                                       {:keys [lines file-name clojure?] :as buffer} (get buffers current-buffer)]
-                                                   (if (and clojure? (= current-tab :files))
-                                                     (do
-                                                       (doto out-pipe
-                                                         (.write (str "(do "
-                                                                      (pr-str '(println))
-                                                                      (pr-str (list 'println "Reloading" file-name))
-                                                                      (str/join \newline lines)
-                                                                      ")\n"))
-                                                         .flush)
-                                                       (assoc state :current-tab :repl-in))
-                                                     state))))))))))
 
 (def keycode->keyword
   {GLFW/GLFW_KEY_BACKSPACE :backspace
@@ -97,34 +40,106 @@
    GLFW/GLFW_KEY_R \R
    GLFW/GLFW_KEY_U \U})
 
-(defn listen-for-keys [window callback vim pipes]
-  (GLFW/glfwSetKeyCallback window
-    (reify GLFWKeyCallbackI
-      (invoke [this window keycode scancode action mods]
-        (when (= action GLFW/GLFW_PRESS)
-          (let [control? (not= 0 (bit-and mods GLFW/GLFW_MOD_CONTROL))
-                alt? (not= 0 (bit-and mods GLFW/GLFW_MOD_ALT))
-                shift? (not= 0 (bit-and mods GLFW/GLFW_MOD_SHIFT))]
-            (if-let [k (keycode->keyword keycode)]
-              (cond
-                (and (or control? alt?) (= k :tab))
-                (vim/open-buffer-for-tab! vim (swap! c/*state c/change-tab (if shift? -1 1)))
-                (and (= k :enter)
-                     (vim/normal-mode? vim)
-                     (= :repl-in (:current-tab @c/*state)))
-                (vim/repl-enter! vim callback pipes)
-                :else
-                (when-let [key-name (vim/keyword->name k)]
-                  (callback key-name)))
-              (when control?
-                (when-let [ch (keycode->char keycode)]
-                  (callback (str "<C-" (when shift? "S-") ch ">")))))))))))
+(defn toggle? [keycode scancode action mods]
+  (and (= action GLFW/GLFW_PRESS)
+       (= keycode GLFW/GLFW_KEY_ESCAPE)
+       (= (:mode @c/*state) 'NORMAL)))
 
-(defn listen-for-chars [window callback]
-  (GLFW/glfwSetCharCallback window
-    (reify GLFWCharCallbackI
-      (invoke [this window codepoint]
-        (callback (str (char codepoint)))))))
+(defn on-mouse-move! [{:keys [game]} window xpos ypos]
+  (as-> (swap! c/*state
+          (fn [state]
+            (let [density-ratio (get-density-ratio window)
+                  x (* xpos density-ratio)
+                  y (* ypos density-ratio)]
+              (c/update-mouse state game x y))))
+        state
+        (GLFW/glfwSetCursor window
+                            (GLFW/glfwCreateStandardCursor
+                              (case (:mouse-type state)
+                                ;:ibeam GLFW/GLFW_IBEAM_CURSOR
+                                :hand GLFW/GLFW_HAND_CURSOR
+                                GLFW/GLFW_ARROW_CURSOR)))))
+
+(defn on-mouse-click! [{:keys [vim pipes game]} window button action mods]
+  (when (and (= button GLFW/GLFW_MOUSE_BUTTON_LEFT)
+             (= action GLFW/GLFW_PRESS))
+    (vim/open-buffer-for-tab! vim (swap! c/*state c/click-mouse game
+                                         (fn [state]
+                                           (let [{:keys [current-tab current-buffer buffers tab->buffer]} state
+                                                 {:keys [out-pipe]} pipes
+                                                 {:keys [lines file-name clojure?] :as buffer} (get buffers current-buffer)]
+                                             (if (and clojure? (= current-tab :files))
+                                               (do
+                                                 (doto out-pipe
+                                                   (.write (str "(do "
+                                                                (pr-str '(println))
+                                                                (pr-str (list 'println "Reloading" file-name))
+                                                                (str/join \newline lines)
+                                                                ")\n"))
+                                                   .flush)
+                                                 (assoc state :current-tab :repl-in))
+                                               state)))))))
+
+(defn on-key! [{:keys [vim pipes send-input!]} window keycode scancode action mods]
+  (when (= action GLFW/GLFW_PRESS)
+    (let [control? (not= 0 (bit-and mods GLFW/GLFW_MOD_CONTROL))
+          alt? (not= 0 (bit-and mods GLFW/GLFW_MOD_ALT))
+          shift? (not= 0 (bit-and mods GLFW/GLFW_MOD_SHIFT))]
+      (if-let [k (keycode->keyword keycode)]
+        (cond
+          (and (or control? alt?) (= k :tab))
+          (vim/open-buffer-for-tab! vim (swap! c/*state c/change-tab (if shift? -1 1)))
+          (and (= k :enter)
+               (vim/normal-mode? vim)
+               (= :repl-in (:current-tab @c/*state)))
+          (vim/repl-enter! vim send-input! pipes)
+          :else
+          (when-let [key-name (vim/keyword->name k)]
+            (send-input! key-name)))
+        (when control?
+          (when-let [ch (keycode->char keycode)]
+            (send-input! (str "<C-" (when shift? "S-") ch ">"))))))))
+
+(defn on-resize! [{:keys [game]} window width height]
+  (swap! c/*state
+         (fn [{:keys [current-buffer current-tab tab->buffer] :as state}]
+           (as-> state state
+                 (if current-buffer
+                   (update-in state [:buffers current-buffer] c/update-cursor state game)
+                   state)
+                 ;; if we're in the repl, make sure both the input and output are refreshed
+                 (if-let [other-tab (case current-tab
+                                      :repl-in :repl-out
+                                      :repl-out :repl-in
+                                      nil)]
+                   (update-in state [:buffers (tab->buffer other-tab)] c/update-cursor state game)
+                   state)))))
+
+(defn on-char! [{:keys [send-input!]} window codepoint]
+  (send-input! (str (char codepoint))))
+
+(defn listen-for-events [utils window]
+  (doto window
+    (GLFW/glfwSetWindowSizeCallback
+      (reify GLFWWindowSizeCallbackI
+        (invoke [this window width height]
+          (on-resize! utils window width height))))
+    (GLFW/glfwSetCursorPosCallback
+      (reify GLFWCursorPosCallbackI
+        (invoke [this window xpos ypos]
+          (on-mouse-move! utils window xpos ypos))))
+    (GLFW/glfwSetMouseButtonCallback
+      (reify GLFWMouseButtonCallbackI
+        (invoke [this window button action mods]
+          (on-mouse-click! utils window button action mods))))
+    (GLFW/glfwSetKeyCallback
+      (reify GLFWKeyCallbackI
+        (invoke [this window keycode scancode action mods]
+          (on-key! utils window keycode scancode action mods))))
+    (GLFW/glfwSetCharCallback
+      (reify GLFWCharCallbackI
+        (invoke [this window codepoint]
+          (on-char! utils window codepoint))))))
 
 (defn poll-input [game vim c]
   (async/go-loop [delayed-inputs []]
@@ -151,55 +166,60 @@
   (GLFW/glfwWindowHint GLFW/GLFW_OPENGL_FORWARD_COMPAT GL41/GL_TRUE)
   (GLFW/glfwWindowHint GLFW/GLFW_OPENGL_PROFILE GLFW/GLFW_OPENGL_CORE_PROFILE)
   (GLFW/glfwWindowHint GLFW/GLFW_TRANSPARENT_FRAMEBUFFER GLFW/GLFW_TRUE)
-  (when-let [window (GLFW/glfwCreateWindow 1024 768 "Paravim" 0 0)]
-    (GLFW/glfwMakeContextCurrent window)
-    (GLFW/glfwSwapInterval 1)
-    (GL/createCapabilities)
-    window))
+  (if-let [window (GLFW/glfwCreateWindow 1024 768 "Paravim" 0 0)]
+    (do
+      (GLFW/glfwMakeContextCurrent window)
+      (GLFW/glfwSwapInterval 1)
+      (GL/createCapabilities)
+      window)
+    (throw (Exception. "Failed to create window"))))
 
-(defn ->game [window vim vim-chan]
-  (let [initial-game (assoc (pc/->game window)
-                            :delta-time 0
-                            :total-time 0)
-        send-input! (if vim-chan
-                      (partial async/put! vim-chan)
-                      #(vim/on-input initial-game vim %))
-        pipes (repl/create-pipes)]
-    (listen-for-resize window initial-game)
-    (listen-for-mouse window initial-game vim pipes)
-    (listen-for-keys window send-input! vim pipes)
-    (listen-for-chars window send-input!)
-    (c/init initial-game)
-    (when vim-chan
-      (poll-input initial-game vim vim-chan))
-    (vim/init vim (fn [buffer-ptr event]
-                    (case event
-                      EVENT_BUFENTER
-                      (when vim/*update-ui?*
-                        (vim/on-buf-enter initial-game vim buffer-ptr))
-                      nil)))
-    (when vim-chan
-      (if-let [buffer (-> @c/*state :tab->buffer :repl-out)]
-        (repl/start-repl-thread! nil pipes #(async/put! vim-chan {:buffer buffer :string %}))
-        (throw (ex-info "REPL output buffer not found" {}))))
-    initial-game))
+(defn init
+  ([game]
+   (init game (vim/->vim) (async/chan)))
+  ([game vim vim-chan]
+   (let [send-input! (if vim-chan
+                       (partial async/put! vim-chan)
+                       #(vim/on-input game vim %))
+         pipes (repl/create-pipes)]
+     (c/init game)
+     (when vim-chan
+       (poll-input game vim vim-chan))
+     (vim/init vim (fn [buffer-ptr event]
+                     (case event
+                       EVENT_BUFENTER
+                       (when vim/*update-ui?*
+                         (vim/on-buf-enter game vim buffer-ptr))
+                       nil)))
+     (when vim-chan
+       (if-let [buffer (-> @c/*state :tab->buffer :repl-out)]
+         (repl/start-repl-thread! nil pipes #(async/put! vim-chan {:buffer buffer :string %}))
+         (throw (ex-info "REPL output buffer not found" {}))))
+     {:pipes pipes
+      :send-input! send-input!
+      :vim vim
+      :game game})))
+
+(defn start [game window]
+  (let [game (assoc game :delta-time 0 :total-time 0)
+        utils (init game)]
+    (GLFW/glfwShowWindow window)
+    (listen-for-events utils window)
+    (loop [game game]
+      (when-not (GLFW/glfwWindowShouldClose window)
+        (let [ts (GLFW/glfwGetTime)
+              game (assoc game
+                          :delta-time (- ts (:total-time game))
+                          :total-time ts)
+              game (c/tick game)]
+          (GLFW/glfwSwapBuffers window)
+          (GLFW/glfwPollEvents)
+          (recur game))))
+    (Callbacks/glfwFreeCallbacks window)
+    (GLFW/glfwDestroyWindow window)
+    (GLFW/glfwTerminate)))
 
 (defn -main [& args]
-  (if-let [window (->window)]
-    (do
-      (GLFW/glfwShowWindow window)
-      (loop [game (->game window (vim/->vim) (async/chan))]
-        (when-not (GLFW/glfwWindowShouldClose window)
-          (let [ts (GLFW/glfwGetTime)
-                game (assoc game
-                            :delta-time (- ts (:total-time game))
-                            :total-time ts)
-                game (c/tick game)]
-            (GLFW/glfwSwapBuffers window)
-            (GLFW/glfwPollEvents)
-            (recur game))))
-      (Callbacks/glfwFreeCallbacks window)
-      (GLFW/glfwDestroyWindow window)
-      (GLFW/glfwTerminate))
-    (throw (Exception. "Failed to create window"))))
+  (let [window (->window)]
+    (start (pc/->game window) window)))
 
