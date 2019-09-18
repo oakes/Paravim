2cd76564fb713887b988142dd7e265d8c8981707
oakes <zach@sekao.net>
2019-09-18 13:51:02 -0400

    Increment version number

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject paravim "0.4.2-SNAPSHOT"
+(defproject paravim "0.4.2"
   :description "A parasitic editor for Clojure"
   :url "https://github.com/oakes/Paravim"
   :license {:name "Public Domain"
