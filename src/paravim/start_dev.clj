62ed027f448de64e0500043850b304c3115700d7
oakes <zach@sekao.net>
2020-06-09 16:48:56 -0400

    Move interns to the top level so they are run when ns is reloaded

--- src/paravim/start_dev.clj
+++ src/paravim/start_dev.clj
@@ -6,22 +6,21 @@
             [expound.alpha :as expound]
             [clojure.spec.alpha :as s]))
 
+;; change init and tick to work better with hot code reloading
+(let [init c/init
+      tick c/tick]
+  (intern 'paravim.core 'init (fn [game]
+                                (doto (init game)
+                                  vim/init)))
+  (intern 'paravim.core 'tick (fn [game]
+                                (try
+                                  (tick game)
+                                  (catch Exception e
+                                    (.printStackTrace e)
+                                    game)))))
+
 (defn start []
-  ;; enable spec
   (st/instrument)
   (set! s/*explain-out* expound/printer)
-  ;; change init and tick to work better with hot code reloading
-  (let [init c/init
-        tick c/tick]
-    (intern 'paravim.core 'init (fn [game]
-                                  (doto (init game)
-                                    vim/init)))
-    (intern 'paravim.core 'tick (fn [game]
-                                  (try
-                                    (tick game)
-                                    (catch Exception e
-                                      (.printStackTrace e)
-                                      game)))))
-  ;; start the app
   (start/-main))
 
