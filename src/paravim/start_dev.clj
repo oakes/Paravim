bd3690fd9a8d4a7026f54d04dbaefb233a2e1861
oakes <zach@sekao.net>
2020-06-29 18:39:29 -0400

    Remove merge-into-session test code

--- src/paravim/start_dev.clj
+++ src/paravim/start_dev.clj
@@ -1,16 +1,8 @@
 (ns paravim.start-dev
   (:require [paravim.start :as start]
-            [paravim.core :as c]
-            [paravim.vim :as vim]
-            [paravim.session :as session]
             [orchestra.spec.test :as st]
             [expound.alpha :as expound]
-            [clojure.spec.alpha :as s])
-  (:import [paravim.session Font]))
-
-(session/merge-into-session {:font-changed
-                             (let [font Font]
-                               (println font))})
+            [clojure.spec.alpha :as s]))
 
 (defn start []
   (st/instrument)
