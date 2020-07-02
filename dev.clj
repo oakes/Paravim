c740d76a1e93aad8b21571cc51f306ce90d4ba86
oakes <zach@sekao.net>
2020-07-02 08:19:27 -0400

    Remove unused code

--- dev.clj
+++ dev.clj
@@ -5,15 +5,6 @@
   (println "Unknown task:" task-name)
   (System/exit 1))
 
-(require
-  '[clojure.java.io :as io])
-
-(defn delete-children-recursively! [f]
-  (when (.isDirectory f)
-    (doseq [f2 (.listFiles f)]
-      (delete-children-recursively! f2)))
-  (when (.exists f) (io/delete-file f)))
-
 (require '[paravim.start-dev])
 
 (defmethod task "native"
