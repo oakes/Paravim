74a51261d123b2793c182249deca3b5b284b56c4
oakes <zach@sekao.net>
2020-07-06 14:30:04 -0400

    Start 0.16.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject paravim "0.15.0"
+(defproject paravim "0.16.0-SNAPSHOT"
   :description "A parasitic editor for Clojure"
   :url "https://github.com/oakes/Paravim"
   :license {:name "Public Domain"
